fd7957b588ba1dd91d99d5d06ce485165351e620
reethuo <152209084+reethuo@users.noreply.github.com>
2025-08-12 17:57:48 +0530

    Update infra-vm.tf

--- infra-vm.tf
+++ infra-vm.tf
@@ -2,7 +2,10 @@ provider "google" {
   project = "ritu-pro"
   region  = "northamerica-south1"  # Replace with your desired region
 }
-
+resource "tls_private_key" "my_ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 
 resource "google_compute_instance" "vm_instance" {
   name         =  "vm1"
@@ -22,8 +25,14 @@ resource "google_compute_instance" "vm_instance" {
       # Ephemeral external IP
     }
   }
+metadata = {
+    ssh-keys = "reethu2302:${tls_private_key.my_ssh_key.public_key_openssh}"
+  }
 }
-
+output "private_key" {
+  value     = tls_private_key.my_ssh_key.private_key_pem
+  sensitive = true
+}
 output "instance_private_ip" {
   value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
 }
